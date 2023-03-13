e72588391a340b10a2ef84b4bba6aeda800756a3
Benjamin-DeBruin <benjamintdebruin@gmail.com>
2023-03-13 14:22:56 -0400

    Implemented Input Delay
    Implementing input delay. Yields unexplained error. Reverting to   previous branch to determine if error exists independently or was introduced in these changes

--- Scripts/Tile.cs
+++ Scripts/Tile.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 using UnityEngine;
 using TMPro;
 
+
 public class Tile : MonoBehaviour
 {
     public TileState state {get; private set;}
@@ -39,11 +40,26 @@ public class Tile : MonoBehaviour
         transform.position = cell.transform.position;
     }
 
-    public void MoveTo(TileCell cell){
+    public void MoveTo(TileCell cell){ //Move to destination
         
         this.cell.tile = null;
         this.cell = cell;
         this.cell.tile = this;
-        transform.position = cell.transform.position;
+        StartCoroutine(Animate(cell.transform.position));
+        
+    }
+
+    private IEnumerator Animate(Vector3 to){
+        float elapsed = 0f;
+        float duration = 0.1f;
+
+        Vector3 from = transform.position;
+        while(elapsed <duration){
+            transform.position = Vector3.Lerp (from, to, elapsed/duration); //Move from "from" to "to" by an amount equal to the percent of the overall move
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = to;
     }
 }
--- Scripts/TileBoard.cs
+++ Scripts/TileBoard.cs
@@ -7,7 +7,7 @@ public class TileBoard : MonoBehaviour
 {
     private TileGrid grid;
     private List<Tile> tiles;
-
+    private bool waiting;
     public Tile tilePrefab;
     public TileState[] tileStates;
     private void Awake(){
@@ -17,14 +17,16 @@ public class TileBoard : MonoBehaviour
     }
 
     private void Start(){
-        CreateTile(0);
-        CreateTile(1);
-        CreateTile(2);
-        CreateTile(3);
-        CreateTile(4);
-        CreateTile(5);
-        CreateTile(6);
-        CreateTile(7);
+        /**
+            CreateTile(0);
+            CreateTile(1);
+            CreateTile(2);
+            CreateTile(3);
+            CreateTile(4);
+            CreateTile(5);
+            CreateTile(6);
+            CreateTile(7);
+        */
     }
 
     private void CreateTile(){
@@ -43,42 +45,47 @@ public class TileBoard : MonoBehaviour
     }
 
     private void Update(){
-
-        if(Input.GetKeyDown(KeyCode.W)||Input.GetKeyDown(KeyCode.UpArrow)){
-            MoveTiles(Vector2Int.up, 0,1,1,1);
+        
+        if(!waiting){
+            if(Input.GetKeyDown(KeyCode.W)||Input.GetKeyDown(KeyCode.UpArrow)){
+                MoveTiles(Vector2Int.up, 0,1,1,1);
+            }
+            else if(Input.GetKeyDown(KeyCode.S)||Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                MoveTiles(Vector2Int.down, 0,1,grid.height-2,-1);
+            }
+            else if(Input.GetKeyDown(KeyCode.A)||Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                MoveTiles(Vector2Int.left, 0,1,1,1);
+            }
+            else if(Input.GetKeyDown(KeyCode.D)||Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                MoveTiles(Vector2Int.right, grid.width-2,-1,1,1);
+            }
         }
-        else if(Input.GetKeyDown(KeyCode.S)||Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            MoveTiles(Vector2Int.down, 0,1,grid.height-2,-1);
-        }
-        else if(Input.GetKeyDown(KeyCode.A)||Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            MoveTiles(Vector2Int.left, 0,1,1,1);
-        }
-        else if(Input.GetKeyDown(KeyCode.D)||Input.GetKeyDown(KeyCode.RightArrow))
-        {
-             MoveTiles(Vector2Int.right, grid.width-2,-1,1,1);
-        }
+        
 
 
     }
 
     private void MoveTiles(Vector2Int direction, int startX, int incrementX, int startY, int incrementY){
-        
+        bool stateChanged = false;
         for(int x = startX; x >=0 && x < grid.width; x+=incrementX){
             for(int y = startY; y >=0 && y < grid.height; y+=incrementY){
                 TileCell cell = grid.GetCell(x,y);
                 
                 if(cell.occupied){
-                    MoveTile(cell.tile,direction);
+                    stateChanged |= MoveTile(cell.tile,direction);
                 }
             }   
         }
 
-
+        if(stateChanged){
+            StartCoroutine(WaitForChanges());
+        }
     }
 
-    private void MoveTile(Tile tile, Vector2Int direction){
+    private bool MoveTile(Tile tile, Vector2Int direction){
         TileCell newCell = null;
         TileCell adjacent = grid.GetAdjacentCell(tile.cell, direction);
                             
@@ -97,10 +104,19 @@ public class TileBoard : MonoBehaviour
 
         if(newCell!= null){
             tile.MoveTo(newCell);
+            return true;
         }
-                
+        return false;  
     }
 
+    private IEnumerator WaitForChanges(){
+        waiting = true;
+
+        yield return new WaitForSeconds(0.1f);
+
+        waiting = false;
+        //TODO: create new Tiles and check board state
+    }
 
 
 }
